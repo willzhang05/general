71245fc838777cdc369209a1fe166de013a70749
William Zhang <willzhang05@gmail.com>
2018-10-31 20:21:22 +0000

    cleanup

---
+++ examples/testSlurm.stag.stag_.sh
@@ -0,0 +1,2 @@
+cp local:/nfs/projectSlurm/testSlurm.stag_.sh rns:/tmp/CCC/UVA/xcgbes/rce-slurm-front.novalocal02958610/testSlurm.stag_.sh
+cp local:/nfs/projectSlurm/testSlurm.stag_.sh rns:/tmp/CCC/UVA/xcgbes/rce-slurm-front.novalocal02958610/testSlurm.stag_.sh
