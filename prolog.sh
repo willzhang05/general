26477355b8a176aa1cd761c532eade901a756773
William Zhang <willzhang05@gmail.com>
2018-11-01 22:01:08 +0000

    add changes done with grimshaw

--- prolog.sh
+++ prolog.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-/bin/echo $SLURM_JOB_NAME >> /nfs/testing.txt
-export SLURM_JOB_SCONTROL_INFO=$(/usr/bin/scontrol show job $SLURM_JOB_ID)
-export SLURM_JOB_DIR=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "WorkDir" | cut -d '=' -f 2-)
-export SLURM_JOB_COMMAND=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "Command" | cut -d '=' -f 2-)
-export SLURM_JOB_STDIN=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "StdIn" | cut -d '=' -f 2-)
-export SLURM_JOB_STDERR=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "StdErr" | cut -d '=' -f 2-)
-export SLURM_JOB_STDOUT=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "StdOut" | cut -d '=' -f 2-)
-/bin/echo $SLURM_JOB_DIR > /nfs/jobinfo.txt
-/bin/echo $SLURM_JOB_COMMAND >> /nfs/jobinfo.txt
-/bin/echo $SLURM_JOB_STDIN >> /nfs/jobinfo.txt
-/bin/echo $SLURM_JOB_STDERR >> /nfs/jobinfo.txt
-/bin/echo $SLURM_JOB_STDOUT >> /nfs/jobinfo.txt
 
-
+/nfs/projectSlurm/callCCC > /nfs/cccoutput.txt
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+/bin/echo $SLURM_JOB_NAME > /nfs/jobinfo.txt
+export SLURM_JOB_DIR=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "WorkDir" | cut -d '=' -f 2- )
+export SLURM_JOB_COMMAND=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "Command" | cut -d '=' -f 2- )
+export SLURM_JOB_STDIN=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "StdIn" | cut -d '=' -f 2- )
+export SLURM_JOB_STDERR=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "StdErr" | cut -d '=' -f 2- )
+export SLURM_JOB_STDOUT=$( /usr/bin/scontrol show job $SLURM_JOB_ID | grep -i "StdOut" | cut -d '=' -f 2- )
+/bin/echo $SLURM_JOB_DIR >> /nfs/jobinfo.txt
+/bin/echo $SLURM_JOB_COMMAND >> /nfs/jobinfo.txt
+/bin/echo $SLURM_JOB_STDIN >> /nfs/jobinfo.txt
+/bin/echo $SLURM_JOB_STDERR >> /nfs/jobinfo.txt
+/bin/echo $SLURM_JOB_STDOUT >> /nfs/jobinfo.txt
+
